04e5c41fc356fcb554b6a66d67128183e2f94618
gjbp10 <144659102+gjbp10@users.noreply.github.com>
2025-09-15 20:09:34 +0800

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -26,7 +26,7 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData)
     if (token) {
       try {
-        await axios.post(backendUrl + "/api/cart/add", { itemId, size }, { headers: { token } })
+        await axios.post(backendUrl + "/api/cart/add", { itemId }, { headers: { token } })
       } catch (error) {
         console.log(error)
         toast.error(error.message)
