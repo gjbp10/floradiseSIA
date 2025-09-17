f055b4ea42031087a97dcc9842b8991ed398184a
Aubrey Tavu <qadctavu@tip.edu.ph>
2025-09-17 16:03:39 +0800

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
@@ -7,6 +8,8 @@ import List from './pages/List'
 import Orders from './pages/Orders'
 import Inventory from './pages/Inventory'
 import Login from './components/Login'
+import User from './pages/User'
+import Profile from './pages/Profile' 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -15,35 +18,37 @@ export const currency = '₱'
 
 const App = () => {
 
-  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
 
-  useEffect(() => {
-    localStorage.setItem('token', token)
-  }, [token])
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  }, [token])
 
-  return (
-    <div className='bg-gray-50 min-h-screen'>
-      <ToastContainer />
-      {token === ""
-        ? <Login setToken={setToken} />
-        : <>
-          <Navbar setToken={setToken} />
-          <hr />
-          <div className='flex w-full'>
-            <Sidebar />
-            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
-              <Routes>
-                <Route path='/add' element={<Add token={token} />} />
-                <Route path='/list' element={<List token={token} />} />
-                <Route path='/orders' element={<Orders token={token} />} />
-                <Route path='/inventory' element={<Inventory token={token} />} />
-              </Routes>
-            </div>
-          </div>
-        </>
-      }
-    </div>
-  )
+  return (
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer />
+      {token === ""
+        ? <Login setToken={setToken} />
+        : <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add token={token} />} />
+                <Route path='/list' element={<List token={token} />} />
+                <Route path='/orders' element={<Orders token={token} />} />
+                <Route path='/inventory' element={<Inventory token={token} />} />
+                <Route path='/user' element={<User token={token} />} />
+                <Route path='/profile' element={<Profile token={token} />} /> 
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
+    </div>
+  )
 }
 
 export default App
