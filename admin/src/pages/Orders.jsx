0a993a15804a5e22b83d8c6aa3b9caf9e7d8feb8
Aubrey Tavu <qadctavu@tip.edu.ph>
2025-09-17 19:27:01 +0800

    Update Orders.jsx

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -304,10 +304,6 @@ const Orders = ({ token }) => {
                     <p>
                       {order.address.city +
                         ', ' +
-                        order.address.state +
-                        ', ' +
-                        order.address.country +
-                        ', ' +
                         order.address.zipcode}
                     </p>
                   </div>
