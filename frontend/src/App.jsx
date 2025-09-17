3c280631ef925a41d3f0c060c6096d3efd67c57c
Aubrey Tavu <qadctavu@tip.edu.ph>
2025-09-17 16:06:24 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Wishlist from './pages/Wishlist'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
@@ -33,6 +34,7 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/place-order' element={<PlaceOrder />} />
         <Route path='/orders' element={<Orders />} />
+        <Route path='/wishlist' element={<Wishlist />} />
         <Route path='/verify' element={<Verify />} />
         <Route path='/profile' element={<Profile />} /> 
       </Routes>
