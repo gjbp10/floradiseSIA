f924b65bb1a2c3e5fa1b6fa9684e10b3683e183c
Aubrey Tavu <qadctavu@tip.edu.ph>
2025-10-01 08:52:50 +0800

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-  productId: { type: String }, 
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: Array, required: true },
-  category: { type: String, required: true },
-  subCategory: { type: String, required: true },
-  sizes: { type: Array, required: true },
-  bestseller: { type: Boolean },
-  date: { type: Number, required: true },
-  sku: { type: String, required: true, unique: true } 
+  sku: { type: String, unique: true, required: true }, 
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  sizes: { type: Array },
+  bestseller: { type: Boolean },
+  date: { type: Number, required: true }
 });
 
 const productModel = mongoose.models.product || mongoose.model("product", productSchema);
-
 export default productModel;
